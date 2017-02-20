888884142359781db1d25d17687ecce8c03091a2
mizutombo <thomas.shultz52@gmail.com>
2017-02-19 22:12:56 -0800

    adds content to README.md ... continues debug of tests

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# logger
+# body-parser
+
+This app is an Express middleware body-parser.
+```
+1.)  The body-parser reads a request stream in it's entirety into a variable ...
+				*  using data and end events 
+2.)  If there is no body ...
+				*  it calls next()
+3.)  If there is a body ...
+				*  it uses JSON.parse to turn the body into js object
+        *  it then appends to req (req.body = JSON.parse(body))
+        *  then it calls next()
+```				
+For mocking req:
+```
+*  const EventEmitter = require('events')
+*  const req = new EventEmitter()
+```
+**  Mock stream by calling:
+				*  req.emit('data', JSON.stringify(mock_data))
+				*  req.emit('end') (you'll need to call this for no body too)
--- test/body-parser.test.js
+++ test/body-parser.test.js
@@ -4,31 +4,32 @@ const EventEmitter = require('events');
 
 describe('tests body parser middleware :', () => {
 
-	it('reads request stream and returns body, then calls "next()"', done => {
+	it('reads request stream and returns body, then calls "next()"', () => {
+
+		const parser = bodyParser();
+		const req = new EventEmitter();
+		const next = () => {};
+		// let cat = {
+		// 	name: mocha
+		// };
+		let body = '';
+		let cat = 'mocha';
+		parser(req, null, next);
+		req.emit('data', JSON.stringify(cat));	
+		req.emit('end');
+		// assert.deepEqual(req.body, cat);
+		assert.deepEqual(body, cat);
+		next();
+	});
+
+	it('if no body, then calls "next()"', () => {
 
 		const parser = bodyParser();
 		const req = new EventEmitter();
 		const next = () => {
 			// done();
 		};
-		let cat = {
-			"name": "mocha"
-		};
-		parser(req, null, next);
-		req.emit('data', JSON.stringify(cat));	
-		req.emit('end');
-		assert.deepEqual(req.body, cat);
-		done();
-	});
-
-	it('if no body, then calls "next()"', done => {
-
-		const parser = bodyParser();
-		const req = new EventEmitter();
-		const next = () => {
-			done();
-		};
-		let cat = {};
+		let cat = '';
 		parser(req, null, next);
 		req.emit('data', cat );
 		req.emit('end');
@@ -36,7 +37,7 @@ describe('tests body parser middleware :', () => {
 		console.log('body ', (req.body.length));
 
 		assert.deepEqual(req.body, null);
-		done();
+		next();
 	});
 
 });
